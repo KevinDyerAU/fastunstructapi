03b09f85aff70acd96c441fb71b31b28d60bfcde
KevinDyerAU <kdyer1000@gmail.com>
2024-09-30 19:15:36 +1000

    implement synch unstructured api

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from typing import Optional
-
+# from typing import Optional
 import os
 
 from unstructured_ingest.v2.pipeline.pipeline import Pipeline
@@ -12,7 +11,7 @@ from unstructured_ingest.v2.processes.connectors.fsspec.s3 import (
 )
 from unstructured_ingest.v2.processes.partitioner import PartitionerConfig
 from unstructured_ingest.v2.processes.chunker import ChunkerConfig
-from unstructured_ingest.v2.processes.embedder import EmbedderConfig
+# from unstructured_ingest.v2.processes.embedder import EmbedderConfig
 from unstructured_ingest.v2.processes.connectors.fsspec.s3 import (
     S3ConnectionConfig,
     S3AccessConfig,
@@ -28,6 +27,56 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+
+# async def read_item(folder: str):
+#     background_tasks.add_task(process_files, folder)
+#     background_tasks.add_task(startPipeline, folder)
+#     return {"message": "File processing started"}
+
+# def process_files(folder: str):
+#     # Simulate image processing
+#     time.sleep(2)
+#     # Actual file processing would go here
+#     print(f"Processed files "+folder)
+
+# @app.post("/process-async")
+# async def process_async(folder: str, background_tasks: BackgroundTasks):
+#     background_tasks.add_task(process_files, folder)
+#     background_tasks.add_task(startPipeline, folder)
+#     return {"message": "File processing started"}
+
+# Chunking and embedding are optional.
+@app.get("/folder/{folder}")
+def startPipeline(folder: str):
+    Pipeline.from_configs(
+        context=ProcessorConfig(),
+        indexer_config=S3IndexerConfig(remote_url='s3://smartrtobucket/'+folder),
+        downloader_config=S3DownloaderConfig(download_dir="/work"),
+        source_connection_config=S3ConnectionConfig(
+            access_config=S3AccessConfig(
+                key=os.getenv('AWS_S3_KEY'),
+                secret=os.getenv('AWS_S3_SECRET')
+            )
+        ),
+        partitioner_config=PartitionerConfig(
+            partition_by_api=True,
+            api_key=os.getenv('UNSTRUCT_API_KEY'),
+            partition_endpoint='https://api.unstructuredapp.io/general/v0/general',
+            strategy="hi_res",
+            additional_partition_args={
+                "split_pdf_page": True,
+                "split_pdf_allow_failed": True,
+                "split_pdf_concurrency_level": 15
+            }
+        ),
+
+        chunker_config=ChunkerConfig(chunking_strategy="by_title"),
+        destination_connection_config=S3ConnectionConfig(
+            access_config=S3AccessConfig(
+                key=os.getenv('AWS_S3_KEY'),
+                secret=os.getenv('AWS_S3_SECRET')
+            )
+        ),
+        uploader_config=S3UploaderConfig(remote_url='s3://smartrtobucket/'+folder)
+    ).run()
+    return {"message": "File processing started"}
