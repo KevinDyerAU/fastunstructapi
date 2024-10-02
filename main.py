6330548e5fcddd6beca95bf2646e6554a5b4761d
KevinDyerAU <kdyer1000@gmail.com>
2024-10-03 04:55:53 +1000

    refactor to flask

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import os
 
 from unstructured_ingest.v2.pipeline.pipeline import Pipeline
@@ -18,52 +17,55 @@ from unstructured_ingest.v2.processes.connectors.fsspec.s3 import (
     S3UploaderConfig
 )
 
-from fastapi import FastAPI, BackgroundTasks
+from crypt import methods
+from flask import Flask,jsonify,request
+
+app = Flask(__name__)
+
 from pydantic import BaseModel
 
-app = FastAPI()
-@app.get("/")
+
+@app.route("/")
 def root():
-    return {"message": "Hello World"}
-
-
-
-class FileProcess(BaseModel):
-    fileName: str
-
-
+    return jsonify({
+        "Message": "app up and running successfully"
+    })
 
 
 # Chunking and embedding are optional.
-# @app.post("/process_folder/")
-# async def process_folder(file_data: FileProcess, background_tasks: BackgroundTasks):
-#     fileName = file_data.fileName
-#     background_tasks.add_task(startPipeline, fileName)
-#     return {"message": "File processing started"}
-
-@app.post("/process_folder/")
-def process_folder(file_data: FileProcess):
-    fileName = file_data.fileName
-    startPipeline(fileName)
-    return {"message": "File processing completed"}
+@app.route("/access",methods=["POST"])
+async def access():
+    data = request.get_json()
+    fileName = data.get("fileName")
+    awsK = data.get("awsK")
+    awsS = data.get("awsS")
+    unstrK = data.get("unstrK")
 
 
+    await startPipeline(fileName, awsK, awsS, unstrK)
 
-def startPipeline(folder: str):
+    message = f"Filename {fileName} received and processed"
+
+    return jsonify({
+        "Message": message
+    })
+
+
+
+async def startPipeline(folder: str, awsK: str, awsS: str, unstK: str):
     Pipeline.from_configs(
         context=ProcessorConfig(),
-        #indexer_config=S3IndexerConfig(remote_url='s3://smartrtobucket/SmartRTO/'+folder+"/"),
         indexer_config=S3IndexerConfig(remote_url=folder),
         downloader_config=S3DownloaderConfig(),
         source_connection_config=S3ConnectionConfig(
             access_config=S3AccessConfig(
-                key=os.getenv('AWS_S3_KEY'),
-                secret=os.getenv('AWS_S3_SECRET')
+                key=awsK,
+                secret=awsS
             )
         ),
         partitioner_config=PartitionerConfig(
             partition_by_api=True,
-            api_key=os.getenv('UNSTRUCT_API_KEY'),
+            api_key=unstK,
             partition_endpoint='https://api.unstructuredapp.io/general/v0/general',
             strategy="hi_res",
             additional_partition_args={
@@ -76,11 +78,13 @@ def startPipeline(folder: str):
         chunker_config=ChunkerConfig(chunking_strategy="by_title"),
         destination_connection_config=S3ConnectionConfig(
             access_config=S3AccessConfig(
-                key=os.getenv('AWS_S3_KEY'),
-                secret=os.getenv('AWS_S3_SECRET')
+                key=awsK,
+                secret=awsS
             )
         ),
-        #uploader_config=S3UploaderConfig(remote_url='s3://smartrtobucket/'+folder)
         uploader_config=S3UploaderConfig(remote_url=folder)
     ).run()
     
+
+if __name__=="__main__":
+    app.run(debug=True,host="0.0.0.0",port=8080)
