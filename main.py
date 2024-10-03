8ab123f84520a2aded62693c767824380ee9b343
KevinDyerAU <kdyer1000@gmail.com>
2024-10-04 07:53:17 +1000

    wsgi impl gunicorn

--- main.py
+++ main.py
@@ -87,3 +87,5 @@ async def startPipeline(folder: str, awsK: str, awsS: str, unstK: str):
 
 # if __name__=="__main__":
 #     app.run(debug=True,host="0.0.0.0",port=8080)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=80)
