a46471ba3a5e54437c66085b1415a316eaa80d93
KevinDyerAU <kdyer1000@gmail.com>
2025-06-24 17:07:15 +1000

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,12 @@
-from src.main import app
 import os
+import sys
+import pathlib
+
+# Add src directory to Python path
+src_path = str(pathlib.Path(__file__).parent / 'src')
+sys.path.append(src_path)
+
+from main import app
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 80))
