da498d446381be5c66584461d1622cb17a76481b
KevinDyerAU <kdyer1000@gmail.com>
2025-06-24 18:55:08 +1000

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ import sys
 import pathlib
 
 # Add project root and src directory to Python path
-project_root = str(pathlib.Path(__file__).parent.parent)
+project_root = str(pathlib.Path(__file__))
 sys.path.append(project_root)
 sys.path.append(os.path.join(project_root, 'src'))
 
@@ -11,5 +11,4 @@ from main import app
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 80))
-    app.run(host='0.0.0.0', port=port)
     app.run(host='0.0.0.0', port=port)
